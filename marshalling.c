f937292defb4e80e4e2621a7f1a7a1e51004cfe2
Magdalena Vega <mvega@localhost.localdomain>
2016-04-22 14:37:27 -0300

    empeze el marshalling, despues sigo en casa

--- marshalling.c
+++ marshalling.c
@@ -1,14 +1,55 @@
-RequestData marshalling(int direction[]){
-	RequestData * dataS = malloc(sizeof(RequestData));
-	dataS->data = "IZQUIERDA";
-	dataS->size = sizeof(dataS->data);
-	return *dataS;
+#include "marshalling.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void * unmarshalling(char * data){
+	if(data == NULL){
+		return NULL;
+	}
+	if(data[0] == STRING){
+		String * s = malloc(sizeof(String));
+		//HandleString(&s, data);
+		return s;
+
+	}else if(data[0] == POINT){
+		Point * p = malloc(sizeof(Point));
+
+	}else if (data[0] == BOARD){
+		Board * t = malloc(sizeof(Board));
+	}else{
+		//hay error, me mando cualquier cosa
+	}
 }
 
-char * unmarshalling(ResponseData * data){
-	char * dataR=malloc(2*sizeof(char));
-	if(1){
-		//se pregunta para ver que es lo que recibe y modificar dataR
+char * marshPoint(String* s){
+return NULL;
+}
+char * marshBoard(Board* s){
+return NULL;
+}
+char * marshString(Point* s){
+	char * d;
+	int i=0;
+	d = malloc (1 + 1 + 1 + sizeof(s->x) + sizeof(s->y));
+	d[i] = POINT;
+	itoa(s->x,d+1,10);
+
+}
+
+char * marshalling(void * struc, int type){
+	if(type == STRING){
+		return marshString((String * )struc);
+	}else if(type == BOARD){
+		return marshBoard((Board *)struc);
+	}else if(type == POINT){
+		return marshPoint((Point *)struc);
+	}else {
+		return NULL;
 	}
-	return dataR;
+}
+
+
+int main(){
+	
 }
--- marshalling.h
+++ marshalling.h
@@ -1,2 +1,27 @@
-RequestData marshalling(int direction[]);
-char * unmarshalling(ResponseData * data);
+#include <stdio.h>
+
+typedef struct {
+	int x;
+	int y;
+}Point;
+
+typedef struct {
+	char ** tablero;
+	int filas;
+	int columnas;
+}Board;
+
+typedef struct {
+	char * string;
+	int size;
+}String;
+
+
+#define STRING '3'
+#define POINT '1'
+#define BOARD '5'
+#define SEP '#' //el separador
+
+void * unmarshalling(char * data);
+
+char * marshalling(void * struc, int type);
