40ff70ce8134ee71851efe46df2a8e51422ba4d3
LucasCasa <lcasagrande@itba.edu.ar>
2016-05-02 17:01:27 -0300

    cambio com.h

--- com.h
+++ com.h
@@ -12,8 +12,9 @@ int size;
 
 //typedef StreamData (*RequestHandler)(StreamData* req);
 
-Connection* start_ipc(char* addr);
-void close_ipc(Connection * c);
+Connection * connect(char* addr);
+Connection * listen(char* addr);
+void closeConn(Connection * c);
 int  sendData(Connection * connection, StreamData * req);
 Connection* accept(Connection *c);
 //int Response(RequestData * req, ResponseData * resp); CREO QUE ES AL DOPE
@@ -23,3 +24,12 @@ void receiveData(Connection * connection, StreamData * buffer);
 /* habria que hacer una con un select, pero no tengo ni idea de como hacerlo*/
 
 #endif
+
+connectionRequests = listen(addressObject);
+
+while(1) {
+	connection = accept(connectionRequests);
+	read(connection, whatever);
+	response = process(whatever);
+	write(connection, response);
+}
